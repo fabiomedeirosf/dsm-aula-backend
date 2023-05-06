0e26c6f4a41f6ba03ffeeb466b12bbb7a733bd4b
Fabio Faria <fabio.faria@luizalabs.com>
2023-05-05 22:16:23 -0300

    feat: add config routes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const server = require("./server");
+const {server, plugins} = require("./server");
 
 (async () => {
+    await server.register(plugins);
     await server.start();
     console.log('Server started %s', server.info.uri);
     console.log(`Server started ${server.info.uri}`);
